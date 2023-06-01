f49763a9d920dc7d99feab8c78db71f1631da319
alexwholland <alexwholland@gmail.com>
2023-05-31 18:23:10 -0700

    Setup simple API template

--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -4,6 +4,19 @@ from app.algorithm.predict import perform_algorithm
 router = APIRouter()
 
 @router.get("/")
-def class_room(class_size: int = 1000):
-    result = perform_algorithm(class_size)
-    return {"result": result}
+def class_room(semester: str) -> dict:
+    """
+    Get class size for each course for the the given semester.
+
+    Parameters:
+        - semester (str): The semester for which class information is requested. Can be "FALL", "SPRING", or "SUMMER".
+
+    Returns:
+        - dict: A dictionary containing the course, class size, and semester.
+    """
+    class_size = perform_algorithm(semester)
+    return {
+        "course": "csc 115",
+        "size": class_size,
+        "semester": semester
+    }
