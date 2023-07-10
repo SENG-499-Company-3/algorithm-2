8d1b10e6a2b5921b02137970896b921bc4c8ebbf
bhavanvir <bhavanvir.r@gmail.com>
2023-07-10 16:03:21 -0700

    Initial model deployment

--- app/algorithm/predict.py
+++ app/algorithm/predict.py
@@ -1,25 +1,63 @@
-from fastapi import HTTPException
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
+import pandas as pd
+import numpy as np
+import pickle
 
-def perform_algorithm(semester: str) -> int:
-    """
-    Perform the algorithm to determine the class size based on the given semester.
+def load():
+    with open('../data/best_regressor.pkl', 'rb') as f:
+        model = pickle.load(f)
+    
+    with open('../data/lut.pkl', 'rb') as f:
+        lut = pickle.load(f)
+    
+    return model, lut
 
-    Parameters:
-        - semester (str): The semester for which class information is requested. Can be "FALL", "SPRING", or "SUMMER".
+def imputed_value(rows, column):
+    values = np.append(rows[column].values, [np.nan]).reshape(-1, 1)
+    imputer = IterativeImputer(max_iter=10, random_state=42)
+    return imputer.fit_transform(values)[-1][0]
 
-    Returns:
-        - int: The determined class size based on the semester.
+def fill_missing(X, LUT):
+    course = X['Course'].values[0]
+    term = X['Term'].values[0]
 
-    Raises:
-        - HTTPException (status_code=400): If an invalid semester is provided.
-    """
-    if semester == "FALL":
-        class_size = 200
-    elif semester == "SPRING":
-        class_size = 150
-    elif semester == "SUMMER":
-        class_size = 100
-    else:
-        raise HTTPException(status_code=400, detail="Invalid semester")
+    rows = LUT[(LUT['Course'] == course) & (LUT['Term'] == term)]
 
-    return class_size
+    X['TF_IDF'] = imputed_value(rows, 'TF_IDF')
+    X['Cap'] = round(imputed_value(rows, 'Cap'), 0)
+    X['Enrolled Ratio'] = imputed_value(rows, 'Enrolled Ratio')
+
+    X['Dept Desc'] = rows['Dept Desc'].values[0]
+    X['Class Yr'] = rows['Class Yr'].values[0]
+
+    return X
+
+def construct_instance(LUT):
+    X_new = pd.DataFrame({
+        'Course': ['CSC 225'],
+        'TF_IDF': [np.nan],
+        'Term': ['Summer'],	
+        'Term Yr': [2024],	
+        'Class Yr': [np.nan],	
+        'Dept Desc': [np.nan],
+        'Cap': [np.nan],		
+        'Enrolled Ratio': [np.nan]
+    })
+
+    X_filled = fill_missing(X_new, LUT)
+
+    return X_filled
+
+
+def perform_algorithm():
+    model, LUT = load()
+    X = construct_instance(LUT)
+    prediction = round(model.predict(X)[0], 0)
+
+    print(f"For {X['Course'].values[0]} in {X['Term'].values[0]} {X['Term Yr'].values[0]}:\nWe predict {prediction} students will enroll 🎉")
+    
+    return prediction
+
+if __name__ == "__main__":
+    perform_algorithm()
