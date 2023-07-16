3e145a26b88bd32c5820b5bd45bef54b3bcb6624
alexwholland <alexwholland@gmail.com>
2023-07-16 13:46:12 -0700

    Removed prereq/coreq

--- test_endpoint.py
+++ test_endpoint.py
@@ -2,19 +2,16 @@ import sys
 import json
 import unittest
 from app.api.endpoints import predict_class_sizes
-from app.data_model.course import Course, pastEnrol
+from app.data_model.course import Course, pastEnrollment
 
 class api_test(unittest.TestCase):
     def test_predictor_one_class(self):
-        print("testing predictor with one class")
         data = [
             Course(
                 course="CSC 115",
                 term=5,
-                prereq=[],
-                coreq=[],
                 pastEnrol=[
-                    pastEnrol(year=2017, term=5, size=0),
+                    pastEnrollment(year=2024, term=5, size=0),
                 ]
             )
         ]
